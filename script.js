5addaab09267c018edc205a18c0d92ef10798419
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-03-02 01:34:59 -0300

    armazenamento das opcoes escolhidas e envio do objeto para a API, emitindo alerta de resposta conforme recebido

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let selecionado1 = 0;
 let selecionado2 = 0;
 let selecionado3 = 0;
 
+let input = "";
+let model = "";
+let neck = "";
+let material = "";
+
 function selecionarItem1(opcao) {
 
     let removedor = document.querySelector(".selecionado1");
@@ -13,6 +18,7 @@ function selecionarItem1(opcao) {
 
     opcao.classList.add('selecionado1');
     selecionado1 = 1;
+    model = opcao.id;
 
     checarFinalizacao();
 }
@@ -26,6 +32,7 @@ function selecionarItem2(opcao) {
 
     opcao.classList.add('selecionado2');
     selecionado2 = 1;
+    neck = opcao.id;
 
     checarFinalizacao();
 }
@@ -39,12 +46,13 @@ function selecionarItem3(opcao) {
 
     opcao.classList.add('selecionado3');
     selecionado3 = 1;
+    material = opcao.id;
 
     checarFinalizacao();
 }
 
 function checarFinalizacao() {
-    let input = document.querySelector("input");
+    input = document.querySelector("input");
     if (selecionado1 && selecionado2 && selecionado3 && input.value) {
         habilitarConfirmacao();
     }
@@ -65,4 +73,37 @@ function desabilitarConfirmacao() {
     if (botao.classList.contains('ligado')) {
         botao.classList.remove('ligado');
     }
+}
+
+function confirmar() {
+    let botao = document.querySelector('button');
+    if (botao.classList.contains('ligado')) {
+        enviarPedido();
+    }
+}
+
+function enviarPedido() {
+
+    let promise = axios.post(
+        "https://mock-api.driven.com.br/api/v4/shirts-api/shirts",
+        {
+            "model": model,
+            "neck": neck,
+            "material": material,
+            "image": input.value,
+            "owner": nome,
+            "author": nome
+        }
+    );
+
+    promise.then(sucesso);
+    promise.catch(erro);
+}
+
+function sucesso() {
+    alert("Pedido confirmado!");
+}
+
+function erro() {
+    alert("Ops, não conseguimos processar sua encomenda");
 }
